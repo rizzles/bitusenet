968b00ac699658d38a0726bc9a002a7f2a2e053d
rizzles <mike@itchycats.com>
2012-10-03 17:47:52 +0000

    More mixpanel shit

--- static/js/bitusenet.js
+++ static/js/bitusenet.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
 	$(".signuppage").click(function() {
-		mixpanel.track("signup page");
+		mixpanel.track("signup page clicked");
 	    });
+
+	$(".create").click(function() {
+		mixpanel.track("account created");
+	    });
+	
 
     });
